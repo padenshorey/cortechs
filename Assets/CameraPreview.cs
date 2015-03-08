4f2e392f1d64e189b253bd38055260a1f5e1b02f
Paden Shorey <paden.shorey@gmail.com>
2015-03-08 03:39:42 -0400

    Added the second camera, and album preview

---
+++ Assets/CameraPreview.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraPreview : MonoBehaviour {
+
+	private Camera camera;
+	public Camera mainCamera;
+	private float growth = 0.06f;
+
+	// Use this for initialization
+	void Start () {
+		camera = this.gameObject.GetComponent<Camera> ();
+	}
+
+	void LateUpdate() {
+		//Camera cam = camera;
+		//cam.projectionMatrix = mainCamera.projectionMatrix;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("i")) {
+			camera.rect = new Rect(camera.rect.x, camera.rect.y - (growth/2f), camera.rect.width, camera.rect.height + growth);
+			camera.fieldOfView = 60f*camera.rect.height;
+		}
+		
+		if (Input.GetKeyDown ("k")) {
+			camera.rect = new Rect(camera.rect.x, camera.rect.y + (growth/2f), camera.rect.width, camera.rect.height - growth);
+			camera.fieldOfView = 60f*camera.rect.height;
+		}
+
+		if (Input.GetKeyDown ("j")) {
+			camera.rect = new Rect(camera.rect.x + (growth/2f), camera.rect.y, camera.rect.width - growth, camera.rect.height);
+		}
+		
+		if (Input.GetKeyDown ("l")) {
+			camera.rect = new Rect(camera.rect.x - (growth/2f), camera.rect.y, camera.rect.width + growth, camera.rect.height);
+		}
+	}
+
+}
---
+++ Assets/LM_Album.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LM_Album : MonoBehaviour {
+	
+	public float speed = 0.05F;
+	private float startTime;
+	private float journeyLength;
+	private bool lerp = false;
+	private float y = 0;
+	//true = up, false = down
+	private bool direction = true;
+
+	void Start() {
+
+	}
+
+	void Update() {
+
+		if (Input.GetKeyDown ("n")) {
+			StartLerp();
+			direction = true;
+		}
+
+		if (Input.GetKeyDown ("m")) {
+			StartLerp();
+			direction = false;
+		}
+
+		if (lerp) {
+			if(direction)
+				DoLerp (y, 0f);
+			else
+				DoLerp (y, -1f);
+		}
+	}
+
+	public void StartLerp(){
+		startTime = Time.time;
+		journeyLength = Vector3.Distance(new Vector3(0f, -1f, 0f), new Vector3(0f, 0f, 0f));
+		y = this.transform.position.y;
+		lerp = true;
+	}
+
+	public void DoLerp(float starty, float endy){
+		float distCovered = (Time.time - startTime) * speed;
+		float fracJourney = distCovered / journeyLength;
+		transform.position = Vector3.Lerp (new Vector3(0f, starty, 0f), new Vector3(0f, endy, 0f), fracJourney);
+	}
+}
